3fb1250d2c49a15d82a44594a349ee7f9e043d9a
Mahmoud Kamel <kamelmahmoud335@gmail.com>
2025-12-10 02:19:43 +0200

    test1

--- src/components/shared/button-mode-toggle.shared.tsx
+++ src/components/shared/button-mode-toggle.shared.tsx
@@ -1,6 +1,6 @@
-import { useTheme } from "@lib/hooks/use-theme.hook";
 import { Button } from "@components/ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@components/ui/dropdown-menu";
+import { useTheme } from "@lib/hooks/use-theme.hook";
 import { Moon, Sun } from "lucide-react";
 
 export function ButtonModeToggle() {
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
+import "./i18n";
 import App from "@app/app";
 import "@styles/index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./i18n";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
